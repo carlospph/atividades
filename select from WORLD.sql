ab9ba25b4b15d7c7353ec3e5f26572d5f9b1b498
Carlos Nascimento <80932010+carlospph@users.noreply.github.com>
2025-06-02 21:58:09 -0300

    Create select from WORLD.sql

---
+++ select from WORLD.sql
@@ -0,0 +1,10 @@
+1.SELECT name, continent, population FROM world
+
+2. SELECT name FROM world
+WHERE population >= 200000000
+
+3.**SELECT name, per_capita_GDP FROM world WHERE population > 200000000
+
+4. pulei
+
+5.SELECT name, population FROM world WHERE name IN('França','Alemanha','Itália') 
